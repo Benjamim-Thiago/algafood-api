6b769b32c4b1f055c7efad26eeb17cf1476e9b21
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2020-05-13 07:12:20 -0300

    definido timezone na classe started da aplicação

--- src/main/java/br/com/btsoftware/algafood/AlgafoodApiApplication.java
+++ src/main/java/br/com/btsoftware/algafood/AlgafoodApiApplication.java
@@ -1,10 +1,20 @@
 package br.com.btsoftware.algafood;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgafoodApiApplication {
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApiApplication.class, args);

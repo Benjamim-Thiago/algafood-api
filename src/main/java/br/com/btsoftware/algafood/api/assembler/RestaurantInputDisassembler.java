473756d87aa5d049777f8e022e6b36d4919c7da4
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2020-08-17 14:46:18 -0300

    Usando a dependecia ModelMapper

--- src/main/java/br/com/btsoftware/algafood/api/assembler/RestaurantInputDisassembler.java
+++ src/main/java/br/com/btsoftware/algafood/api/assembler/RestaurantInputDisassembler.java
@@ -1,24 +1,18 @@
 package br.com.btsoftware.algafood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.btsoftware.algafood.api.model.input.RestaurantInput;
-import br.com.btsoftware.algafood.domain.model.Kitchen;
 import br.com.btsoftware.algafood.domain.model.Restaurant;
 
 @Component
 public class RestaurantInputDisassembler {
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	public Restaurant toDomainObject(RestaurantInput restaurantInput) {
-		Restaurant restaurant = new Restaurant();
-		restaurant.setName(restaurantInput.getName());
-		restaurant.setDeliveryFee(restaurantInput.getDeliveryFee());
-		
-		Kitchen kitchen = new Kitchen();
-		kitchen.setId(restaurantInput.getKitchen().getId());
-		
-		restaurant.setKitchen(kitchen);
-		
-		return restaurant;
+		return modelMapper.map(restaurantInput, Restaurant.class);
 	}
 }
--- src/main/java/br/com/btsoftware/algafood/api/assembler/RestaurantModelAssembler.java
+++ src/main/java/br/com/btsoftware/algafood/api/assembler/RestaurantModelAssembler.java
@@ -3,26 +3,21 @@ package br.com.btsoftware.algafood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import br.com.btsoftware.algafood.api.model.KitchenModel;
 import br.com.btsoftware.algafood.api.model.RestaurantModel;
 import br.com.btsoftware.algafood.domain.model.Restaurant;
 
 @Component
 public class RestaurantModelAssembler {
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public RestaurantModel toModel(Restaurant restaurant) {
-		KitchenModel kitchenModel = new KitchenModel();
-		kitchenModel.setId(restaurant.getKitchen().getId());
-		kitchenModel.setName(restaurant.getKitchen().getName());
-		
-		RestaurantModel restaurantModel = new RestaurantModel();
-		restaurantModel.setId(restaurant.getId());
-		restaurantModel.setName(restaurant.getName());
-		restaurantModel.setDeliveryFee(restaurant.getDeliveryFee());
-		restaurantModel.setKitchen(kitchenModel);
-		return restaurantModel;
+		return modelMapper.map(restaurant, RestaurantModel.class);
 	}
 	
 	public List<RestaurantModel> toCollectionModel(List<Restaurant> restaurants) {

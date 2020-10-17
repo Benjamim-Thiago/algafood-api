1773475366e2532bbcc10f713302969f41180501
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2020-10-17 15:01:53 -0300

    definido as fields ordenaveis na camada no edpoint pedido

--- src/main/java/br/com/btsoftware/algafood/api/controller/RequestController.java
+++ src/main/java/br/com/btsoftware/algafood/api/controller/RequestController.java
@@ -1,6 +1,7 @@
 package br.com.btsoftware.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -24,6 +25,7 @@ import br.com.btsoftware.algafood.api.assembler.input.RequestInputDisassembler;
 import br.com.btsoftware.algafood.api.model.RequestModel;
 import br.com.btsoftware.algafood.api.model.RequestResumeModel;
 import br.com.btsoftware.algafood.api.model.input.RequestInput;
+import br.com.btsoftware.algafood.core.data.PageableTranslator;
 import br.com.btsoftware.algafood.domain.exception.BusinessException;
 import br.com.btsoftware.algafood.domain.exception.EntityNotExistException;
 import br.com.btsoftware.algafood.domain.model.Request;
@@ -38,57 +40,74 @@ import br.com.btsoftware.algafood.infrastructure.repository.spec.RequestSpecs;
 @RequestMapping(value = "/requests")
 public class RequestController {
 	@Autowired
-    private RequestRepository requestRepository;
-    
-    @Autowired
-    private RequestService requestService;
-    
-    @Autowired
-    private RequestIssueService issueRequestService;
-    
-    @Autowired
-    private RequestResumeModelAssembler requestResumeModelAssembler;
-    
-    @Autowired
-    private RequestModelAssembler requestModelAssembler;
-    
-    @Autowired
-    private RequestInputDisassembler requestInputDisassembler;
-    
-    @GetMapping
-    public Page<RequestResumeModel> search(RequestFilter filter, @PageableDefault(size = 10) Pageable pageable) {
-        Page<Request> allRequests = requestRepository.findAll(RequestSpecs.searchUseFilter(filter), pageable);
-                       
-        List<RequestResumeModel> requestModel = requestResumeModelAssembler.toCollectionModel(allRequests.getContent());
-        
-        Page<RequestResumeModel> requestModelPage = new PageImpl<>(requestModel, pageable, 
+	private RequestRepository requestRepository;
+
+	@Autowired
+	private RequestService requestService;
+
+	@Autowired
+	private RequestIssueService issueRequestService;
+
+	@Autowired
+	private RequestResumeModelAssembler requestResumeModelAssembler;
+
+	@Autowired
+	private RequestModelAssembler requestModelAssembler;
+
+	@Autowired
+	private RequestInputDisassembler requestInputDisassembler;
+
+	@GetMapping
+	public Page<RequestResumeModel> search(RequestFilter filter, @PageableDefault(size = 10) Pageable pageable) {
+
+		pageable = translatePageable(pageable);
+
+		Page<Request> allRequests = requestRepository.findAll(RequestSpecs.searchUseFilter(filter), pageable);
+
+		List<RequestResumeModel> requestModel = requestResumeModelAssembler.toCollectionModel(allRequests.getContent());
+
+		Page<RequestResumeModel> requestModelPage = new PageImpl<>(requestModel, pageable,
 				allRequests.getTotalElements());
-        
-        return requestModelPage;
-    }
-    
-    @GetMapping("/{code}")
-    public RequestModel find(@PathVariable String code) {
-        Request request = requestService.findOrFail(code);
-        
-        return requestModelAssembler.toModel(request);
-    }            
-    
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public RequestModel save(@Valid @RequestBody RequestInput requestInput) {
-        try {
-            Request newRequest = requestInputDisassembler.toDomainObject(requestInput);
 
-            // TODO pegar usuário autenticado
-            newRequest.setClient(new User());
-            newRequest.getClient().setId(1L);
+		return requestModelPage;
+	}
 
-            newRequest = issueRequestService.save(newRequest);
+	@GetMapping("/{code}")
+	public RequestModel find(@PathVariable String code) {
+		Request request = requestService.findOrFail(code);
 
-            return requestModelAssembler.toModel(newRequest);
-        } catch (EntityNotExistException e) {
-            throw new BusinessException(e.getMessage(), e);
-        }
-    }
+		return requestModelAssembler.toModel(request);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public RequestModel save(@Valid @RequestBody RequestInput requestInput) {
+		try {
+			Request newRequest = requestInputDisassembler.toDomainObject(requestInput);
+
+			// TODO pegar usuário autenticado
+			newRequest.setClient(new User());
+			newRequest.getClient().setId(1L);
+
+			newRequest = issueRequestService.save(newRequest);
+
+			return requestModelAssembler.toModel(newRequest);
+		} catch (EntityNotExistException e) {
+			throw new BusinessException(e.getMessage(), e);
+		}
+	}
+
+	private Pageable translatePageable(Pageable apiPageable) {
+		var mapeamento = Map.of(
+				"code", "code", 
+				"restaurant.name", "restaurant.name",
+				"client.name","client.name", 
+				"valueTotal", "valueTotal",
+				"subTotal", "subTotal",
+				"deliveryFee", "deliveryFee",
+				"requestStatus", "requestStatus"
+			);
+
+		return PageableTranslator.translate(apiPageable, mapeamento);
+	}
 }

5b44e99947474a6ac962756353c11910ab01ac22
Benjamim Thiago <bebenjamimthiago@gmail.com>
2021-03-14 23:26:07 -0300

    ajuste para trabalhar com jwt

--- src/main/java/br/com/btsoftware/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/btsoftware/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package br.com.btsoftware.algafood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +26,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.cors()
 			.and()
-				.oauth2ResourceServer().opaqueToken();
+			.oauth2ResourceServer().jwt(); //.oauth2ResourceServer().opaqueToken();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		String key = "Porque Deus tanto amou o mundo que deu o seu Filho Unigênito,"
+				+ "para que todo o que nele crer não pereça, mas tenha a vida eterna. João 3:16 (NVI)";
+		
+		var secretKey = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }

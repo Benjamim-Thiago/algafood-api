169c43a422478cf032f4c848f1c181a088cc6090
Benjamim Thiago <bebenjamimthiago@gmail.com>
2021-03-16 22:05:54 -0300

    configurando para uso de chave publica

--- src/main/java/br/com/btsoftware/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/btsoftware/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package br.com.btsoftware.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 
 @Configuration
@@ -27,16 +22,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.cors()
 			.and()
 			.oauth2ResourceServer().jwt(); //.oauth2ResourceServer().opaqueToken();
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		String key = "Porque Deus tanto amou o mundo que deu o seu Filho Unigênito,"
-				+ "para que todo o que nele crer não pereça, mas tenha a vida eterna. João 3:16 (NVI)";
-		
-		var secretKey = new SecretKeySpec(key.getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }
